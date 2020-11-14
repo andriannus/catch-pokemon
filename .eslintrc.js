6dd987ac1771f90234ba396d212bbf1a03d15544
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-14 14:43:20 +0700

    chore: setup project

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["import", "jsx-a11y", "react", "react-hooks"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/jsx-one-expression-per-line": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+};
